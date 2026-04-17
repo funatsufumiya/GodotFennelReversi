7fe81ff4e837199ecac03ef109d6ccc787dab1e0
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-17 10:40:00 +0900

    chore

--- scripts/watch.py
+++ scripts/watch.py
@@ -2,6 +2,8 @@ from watchfiles import watch
 import subprocess
 from datetime import datetime
 
+print("watching src update (*.fnl only) ...")
+
 for changes in watch("src"):
     for action, path in changes:
         if path.endswith(".fnl"):
