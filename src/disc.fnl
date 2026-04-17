b6fe58636643470804e5f1aa0165dd1839499464
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-17 12:11:17 +0900

    feat: flipped state

--- src/disc.fnl
+++ src/disc.fnl
@@ -3,10 +3,19 @@
    :class_name "Disc"})
 
 (fn Disc.flip [self]
+  (set self.flipped (not self.flipped))
   (self:rotate_x (deg_to_rad 180)))
 
 (fn Disc._ready [self]
   ;(print "disc ready")
+  (set self.flipped false)
   )
+
+(fn Disc.is_black [self]
+  (not self.flipped))
+
+(fn Disc.is_white [self]
+  ; WORKAROUND
+  (not (not self.flipped)))
 
 Disc
--- src/disc.lua
+++ src/disc.lua
@@ -1,7 +1,16 @@
 local Disc = {extends = Node3D, class_name = "Disc"}
 Disc.flip = function(self)
+  self.flipped = not self.flipped
   return self:rotate_x(deg_to_rad(180))
 end
 Disc._ready = function(self)
+  self.flipped = false
+  return nil
+end
+Disc.is_black = function(self)
+  return not self.flipped
+end
+Disc.is_white = function(self)
+  return not not self.flipped
 end
 return Disc
